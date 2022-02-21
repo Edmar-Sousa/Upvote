f509052576bb72bd2a01f2405e14584f1736acab
Edmar_Sousa <61367147+Edmar-Sousa@users.noreply.github.com>
2022-02-20 21:29:02 -0300

    update

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,12 +1,13 @@
 import { initializeApp, FirebaseOptions  } from 'firebase/app'
 
 const firebaseApp : FirebaseOptions = {
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: ''
+    apiKey: "AIzaSyAqtLf-eHDjtV3sfeeQWdNdqNYrnqqoZ_4",
+    authDomain: "vote-app-c436e.firebaseapp.com",
+    databaseURL: "https://vote-app-c436e-default-rtdb.firebaseio.com",
+    projectId: "vote-app-c436e",
+    storageBucket: "vote-app-c436e.appspot.com",
+    messagingSenderId: "175148772868",
+    appId: "1:175148772868:web:9a301b733cb15626f6cf51"
 }
 
 export default initializeApp(firebaseApp)
